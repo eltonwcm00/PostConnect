9231d96bb2a756897da9603c009975b5d2f584a7
eltonwcm00 <eltonwcm00@gmail.com>
2023-03-10 18:47:49 +0800

    faculty login css v2

--- frontend/src/screens/Faculty/FacultyLogin/FacultyLogin.js
+++ frontend/src/screens/Faculty/FacultyLogin/FacultyLogin.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {useNavigate} from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { facultyLogin } from "../../../actions/facultyAction";
 import Loading from "../../../components/Loading";
 import ErrorMessage from "../../../components/ErrorMessage";
 import SuccessMessage from "../../../components/SuccessMessage";
-import FacultyTemplate from "../../../components/FacultyTemplate";
-import "../InputForm.css";
+import "./FacultyLogin.css";
 
 const FacultyLogin = () => {
 
@@ -29,7 +28,7 @@ const FacultyLogin = () => {
     useEffect(() => {
       if (successMsg) {
         const timer = setTimeout(() => {
-          navigate("/");
+          navigate("/facultyHomepage");
         }, 2000);
         return () => clearTimeout(timer);
       }
@@ -41,41 +40,44 @@ const FacultyLogin = () => {
       };
 
     return (
-      <FacultyTemplate>
-        <div className="form-title-desc-container">Details Of The Faculty</div>
-        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-        {successMsg && <SuccessMessage variant="success">{facultyInfo.successMessage}</SuccessMessage>}
-        {loading && <Loading />}
-        <Form className="form" onSubmit={submitHandler}>
-          <Form.Group as={Row} className="mb-4" controlId="formBasicEmail">
-            <Form.Label column sm={2}>Username</Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                value={userNameFac}
-                placeholder="Enter Username"
-                onChange={(e) => setUserNameFac(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
+         <div style={{ display: "flex", height: "100vh", backgroundColor: '#071B63'}}>
+           <Container>
+            <div className="register-form-title-desc-container">Login</div>
+              <center><img src="/image/postconnect.png" alt="PostConnect Logo"/></center>
+              {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+              {successMsg && <SuccessMessage variant="success">{facultyInfo.successMessage}</SuccessMessage>}
+              {loading && <Loading />}
+              <Form className="register-form" onSubmit={submitHandler}>
+                <Form.Group as={Row} className="mb-4" controlId="formBasicEmail">
+                  <Form.Label className="col-register-form-label" column sm={2}>Username</Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="text"
+                      value={userNameFac}
+                      placeholder="Enter the username"
+                      onChange={(e) => setUserNameFac(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
 
-          <Form.Group as={Row} className="mb-4" controlId="formBasicPassword">
-            <Form.Label column sm={2}>Password</Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
+                <Form.Group as={Row} className="mb-4" controlId="formBasicPassword">
+                  <Form.Label className="col-register-form-label" column sm={2}>Password</Form.Label>
+                  <Col sm={10}>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      placeholder="Enter the password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
 
-          <Button className=" mt-4 float-right" variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form >
-      </FacultyTemplate>
+                <Button className=" mt-4 float-right submit-btn" variant="primary" type="submit">
+                  Login
+                </Button>
+              </Form >
+            </Container>
+        </div>        
     );
 }
 
