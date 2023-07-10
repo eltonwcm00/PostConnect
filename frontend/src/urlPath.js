248b019e5a5b73d3856f376709e67309f7fe43bc
eltonwcm00 <eltonwcm00@gmail.com>
2023-07-10 23:17:21 +0800

    production setup (url path amendation)

--- frontend/src/urlPath.js
+++ frontend/src/urlPath.js
@@ -1,2 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/'
-export const BASE_URL_2 = 'http://localhost:5000/'
+// export const BASE_URL_2 = 'http://localhost:5000/'
+
+export const BASE_URL = 'https://post-connect-ucwb.onrender.com/'
+export const BASE_URL_2 = 'https://post-connect-ucwb.onrender.com/'
